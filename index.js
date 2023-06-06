ea6eb98081b8dd28f34c852dd7938ac9dff44f58
Jay Parmar <jay.p@biztechcs.com>
2023-06-06 11:22:32 +0530

    Reducers

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function orderCake() {
     quantity: 1,
   };
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
