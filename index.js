d55bbd67eda642e34e581a7e62d60a5a564ae3ad
Jay Parmar <jay.p@biztechcs.com>
2023-06-06 11:57:05 +0530

    Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 function orderCake() {
@@ -23,3 +26,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log('Update state ', store.getState())
+);
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
