df7ccad033e700551d4b9297c511ab1ec0bbb538
Jay Parmar <jay.p@biztechcs.com>
2023-06-06 14:15:49 +0530

    Nested state

---
+++ nested-state.js
@@ -0,0 +1,44 @@
+const redux = require('redux');
+
+const initialState = {
+  name: 'Jay',
+  address: {
+    street: '123 Main st',
+    city: 'Ahmedabad',
+    state: 'Gujarat',
+  },
+};
+
+const STREET_UPDATED = 'STREET_UPDATED';
+
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(reducer);
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated state', store.getState());
+});
+
+store.dispatch(updateStreet('789 Main st'));
